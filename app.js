18509d7d8c1c48c01be70ada96b58601bd221012
ritsuki-i <ritsuritsu143@gmail.com>
2023-02-11 03:37:01 +0900

    contact機能実装

--- app.js
+++ app.js
@@ -9,3 +9,32 @@ $(document).on("change", "#sitelink", function () {
       $(".footer").outerHeight(90);
     } 
   });
+
+function reset() {
+  document.contact.reset();
+}
+
+$(function() {
+  $('button[type=submit]').on('click', function() {
+
+    // Formに入力されたデータを取得
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+
+    // 連想配列化
+    var postData = {"name":name, "email":email, "message":message};
+
+    // POSTで送信
+    $.post(
+      "submit.php",
+      postData,
+    );
+
+    // 送信完了後の処理（送信完了のメッセージ表示）
+    alert("送信完了しました。お問い合わせありがとうございます。")
+    $('#name').val("");
+    $('#email').val("");
+    $('#message').val("");
+  });
+});
