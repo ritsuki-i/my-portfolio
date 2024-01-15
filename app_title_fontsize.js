aa11cf568eadff91fe0198ce17a95d2339854b42
ritsuki-i <ritsuritsu143@gmail.com>
2024-01-15 14:00:02 +0900

    change app-title-fontSize

--- app_title_fontsize.js
+++ app_title_fontsize.js
@@ -1,13 +1,21 @@
-window.addEventListener("resize", function () {
-  if (window.innerWidth <= 960) {
-    const playbtn = document.querySelectorAll(".long");
-    const appimg = document.querySelectorAll(".appimg");
-    for (let i = 0; i < playbtn.length; i++) {
-      let fontSize = parseFloat(window.getComputedStyle(playbtn[i]).fontSize);
-      if (playbtn[i].textContent.length * fontSize > playbtn[i].offsetWidth - appimg[i].offsetWidth) {
+'resize DOMContentLoaded'.split(' ').forEach((event)=>{
+  window.addEventListener(event, function () {
+    if (window.innerWidth <= 960) {
+      console.log(window.innerWidth);
+      const playbtn = document.querySelectorAll(".long");
+      const appimg = document.querySelectorAll(".appimg");
+      for (let i = 0; i < playbtn.length; i++) {
+        let fontSize = parseFloat(window.getComputedStyle(playbtn[i]).fontSize);
         size = (playbtn[i].offsetWidth - appimg[i].offsetWidth) / playbtn[i].textContent.length;
+        console.log(size);
         playbtn[i].style.fontSize = size + "px";
       }
+    }else{
+      const playbtn = document.querySelectorAll(".long");
+      for (let i = 0; i < playbtn.length; i++) {
+        size = 3;
+        playbtn[i].style.fontSize = size + "vh";
+      }
     }
-  }
+  });
 });
